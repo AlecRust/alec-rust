5b8781195e6922febca7387af669d7c5800630b6
Alec Rust <me@alecrust.com>
2013-01-12 19:15:57 +0000

    Initial commit of Alec Rust WordPress theme - Further optimised images with ImageOptim for Mac - Added .htaccess file to .gitignore

---
+++ wp-content/themes/alecrust/footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Alec_Rust
+ * @since Alec Rust 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'alecrust_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'alecrust' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'alecrust' ); ?>"><?php printf( __( 'Proudly powered by %s', 'alecrust' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
