c348d0281c774f837c7a78acc962ba3682152898
Alec Rust <me@alecrust.com>
2013-03-16 19:32:04 +0000

    Improved comment timestamp output and styling - Added styling for comment "Reply" link

--- wp-content/themes/alecrust/functions.php
+++ wp-content/themes/alecrust/functions.php
@@ -239,11 +239,11 @@ function alecrust_comment( $comment, $args, $depth ) {
                     printf( '<cite class="fn">%1$s</cite>',
                         get_comment_author_link()
                     );
-                    printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+                    printf( '<a href="%1$s" title="%2$s" class="timestamp"><time datetime="%2$s">%3$s</time></a>',
                         esc_url( get_comment_link( $comment->comment_ID ) ),
                         get_comment_time( 'c' ),
                         /* translators: 1: date, 2: time */
-                        sprintf( __( '%1$s at %2$s' ), get_comment_date(), get_comment_time() )
+                        sprintf( __( '%1$s'), get_comment_date(), get_comment_time() )
                     );
                 ?>
             </header>
