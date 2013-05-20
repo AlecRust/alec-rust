5c893ca11726ca4ec550f88ef9d0c06f589841ce
Alec Rust <me@alecrust.com>
2013-05-20 11:45:37 +0100

    Added meta information to attachment page - Improved styling of attachment page image - Removed unnecessary styling from comments.less

--- wp-content/themes/alecrust/image.php
+++ wp-content/themes/alecrust/image.php
@@ -14,6 +14,21 @@ get_header(); ?>
 
             <header class="entry-header">
                 <h1 class="entry-title"><?php the_title(); ?></h1>
+                <p class="entry-meta attachment-meta">
+                    <?php
+                    $metadata = wp_get_attachment_metadata();
+                    printf( __( 'Published at <a href="%3$s" title="Full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s">%8$s</a>', 'rustyrambles' ),
+                        esc_attr( get_the_time() ),
+                        get_the_date(),
+                        esc_url( wp_get_attachment_url() ),
+                        $metadata['width'],
+                        $metadata['height'],
+                        esc_url( get_permalink( $post->post_parent ) ),
+                        esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
+                        get_the_title( $post->post_parent )
+                    );
+                    ?>
+                </p>
             </header>
 
             <div class="entry-content">
