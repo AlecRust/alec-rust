abffb47377aa568a3806effeb83879813a8c49e3
Alec Rust <me@alecrust.com>
2013-07-18 22:48:05 +0100

    Adjusted syntax of meta viewport element

--- wp-content/themes/alecrust/header.php
+++ wp-content/themes/alecrust/header.php
@@ -16,7 +16,7 @@
             bloginfo('description');
         }
         ?>">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/html5shiv.min.js"></script>
