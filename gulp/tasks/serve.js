db039844d50b18123dfb5eb03c2e737ac82509f2
Alec Rust <me@alecrust.com>
2015-08-13 20:21:20 +0100

    Switch local dev URL from http://localhost:8888/alec-rust/ -> http://local.dev:8888/alec-rust/

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var opn =  require('opn');
 
 gulp.task('serve', function () {
-  opn('http://localhost:8888/alec-rust/');
+  opn('http://local.dev:8888/alec-rust/');
 });
