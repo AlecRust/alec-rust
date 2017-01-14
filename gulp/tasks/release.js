b6964ce5d3fc8b14742428ab384a591c18c20f63
Alec Rust <me@alecrust.com>
2017-01-14 16:13:26 +0000

    Add run-sequence to control sequence of gulp tasks

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var git = require('gulp-git');
 var bump = require('gulp-bump');
 var fs = require('fs');
@@ -22,20 +23,36 @@ gulp.task('bump-version', function () {
   gulp.start('default');
 });
 
-gulp.task('commit-changes', ['bump-version'], function () {
+gulp.task('commit-changes', function () {
   var version = getPackageJsonVersion();
   return gulp.src('.')
     .pipe(git.add())
     .pipe(git.commit('Release version ' + version));
 });
 
-gulp.task('create-new-tag', ['commit-changes'], function (cb) {
+gulp.task('create-new-tag', function (cb) {
   var version = getPackageJsonVersion();
   git.tag(version, 'Created Tag for version: ' + version, function (error) {
     if (error) {
       return cb(error);
     }
   });
+});
+
+gulp.task('release', function (callback) {
+  runSequence(
+    'bump-version',
+    'default',
+    'commit-changes',
+    'create-new-tag',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
 });
 
 gulp.task('release', ['create-new-tag']);
