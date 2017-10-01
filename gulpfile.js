e7398352d11da429d9692ae52e977ba69e1b3cbc
Alec Rust <me@alecrust.com>
2017-10-01 19:15:07 +0100

    Minify CSS/JS output, remove from repo
    - Remove `gulp images` task
    - Simplify/tidy up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-cssnano');
-var cache = require('gulp-cache');
-var imagemin = require('gulp-imagemin');
 var bump = require('gulp-bump');
 var touch = require('gulp-touch');
 var concat = require('gulp-concat');
@@ -20,20 +18,6 @@ var customMedia = require('postcss-custom-media');
 var calc = require('postcss-calc');
 var autoprefixer = require('autoprefixer');
 
-var paths = {
-  styles: {
-    src: 'assets/styles/**/*.styl',
-    temp: './.temp'
-  },
-  scripts: {
-    src: 'assets/scripts/**/*.js'
-  },
-  images: {
-    src: 'assets/images/**/*',
-    dest: 'assets/images'
-  }
-};
-
 function getPackageJsonVersion() {
   // Parse the JSON file instead of using require because require
   // caches multiple calls so the version number won't be updated
@@ -41,66 +25,57 @@ function getPackageJsonVersion() {
 }
 
 function compileStylus() {
-  return gulp.src(paths.styles.src)
+  return gulp.src('assets/styles/**/*.styl')
     .pipe(stylint())
     .pipe(stylint.reporter())
     .pipe(stylus())
-    .pipe(gulp.dest(paths.styles.temp));
+    .pipe(gulp.dest('./.temp'));
 }
 
 function bemlint() {
   return gulp.src('./.temp/**/*.css')
     .pipe(clip())
-    .pipe(postcss([
-      bemLinter('suit')
-    ]));
+    .pipe(postcss([bemLinter('suit')]));
 }
 
 function processCss() {
   return gulp.src('./.temp/*.css')
-    .pipe(postcss([
-      atImport(),
-      customProperties(),
-      calc(),
-      customMedia(),
-      autoprefixer()
-    ]))
+    .pipe(
+      postcss([
+        atImport(),
+        customProperties(),
+        calc(),
+        customMedia(),
+        autoprefixer()
+      ])
+    )
+    .pipe(minifyCss())
     .pipe(gulp.dest('./'));
 }
 
 function scripts() {
-  return gulp.src(paths.scripts.src)
+  return gulp.src('assets/scripts/**/*.js')
     .pipe(concat('script.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./'));
 }
 
-function images() {
-  return gulp.src(paths.images.src)
-    .pipe(cache(imagemin({
-      progressive: true,
-      interlaced: true,
-      svgoPlugins: [{ cleanupIDs: false }]
-    })))
-    .pipe(gulp.dest(paths.images.dest));
-}
-
 function watch() {
-  gulp.watch(paths.styles.src, styles);
-  gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.images.src, images);
+  gulp.watch('assets/styles/**/*.styl', styles);
+  gulp.watch('assets/scripts/**/*.js', scripts);
 }
 
 function bumpVersion() {
-  return gulp.src([
-    'package.json',
-    'assets/styles/style.styl',
-    'functions.php',
-  ], { base: './' })
-    .pipe(bump())
-    .pipe(gulp.dest('./'))
-    // Touch the files to ensure changes are
-    // picked up by Git: https://git.io/vMNKZ
-    .pipe(touch());
+  return (
+    gulp.src(['package.json', 'assets/styles/style.styl', 'functions.php'], {
+      base: './'
+    })
+      .pipe(bump())
+      .pipe(gulp.dest('./'))
+      // Touch the files to ensure changes are
+      // picked up by Git: https://git.io/vMNKZ
+      .pipe(touch())
+  );
 }
 
 function commitChanges() {
@@ -112,7 +87,9 @@ function commitChanges() {
 
 function createNewTag(cb) {
   var version = getPackageJsonVersion();
-  return git.tag(version, 'Created Tag for version: ' + version, function (error) {
+  return git.tag(version, 'Created Tag for version: ' + version, function(
+    error
+  ) {
     if (error) {
       return cb(error);
     }
@@ -124,19 +101,31 @@ function pushChanges(cb) {
 }
 
 function createRelease(done) {
-  githubReleaser({
-    type: 'oauth',
-    token: process.env.CONVENTIONAL_GITHUB_RELEASER_TOKEN
-  }, done);
+  githubReleaser(
+    {
+      type: 'oauth',
+      token: process.env.CONVENTIONAL_GITHUB_RELEASER_TOKEN
+    },
+    done
+  );
 }
 
 exports.scripts = scripts;
-exports.images = images;
 
 var styles = gulp.series(compileStylus, bemlint, processCss);
-var build = gulp.series(gulp.parallel(styles, scripts, images));
+var build = gulp.series(gulp.parallel(styles, scripts));
 
 gulp.task('watch', gulp.series(build, watch));
-gulp.task('release', gulp.series(bumpVersion, build, commitChanges, createNewTag, pushChanges, createRelease));
+gulp.task(
+  'release',
+  gulp.series(
+    bumpVersion,
+    build,
+    commitChanges,
+    createNewTag,
+    pushChanges,
+    createRelease
+  )
+);
 gulp.task('build', build);
 gulp.task('default', build);
